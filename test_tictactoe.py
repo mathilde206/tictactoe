e66af4473bb9dd7180161e31bc2c601e8aa8005e
Mathilde Barbier <mathilde206@gmail.com>
2017-11-25 09:33:49 +0100

    Add function to update the board

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -1,21 +1,45 @@
 import unittest
-from tictactoe import make_grid
+from tictactoe import make_grid, update_grid, is_position_valid
 
-class TestTicTacToe(unittest.TestCase):
+class TestTictactoe(unittest.TestCase):
+
     def test_can_create_an_empty_grid(self):
         self.assertEquals(len(make_grid(2,2)),4)
         self.assertEquals(len(make_grid(3,3)), 9)
 
+
     def test_all_coordinates_on_board(self):
-        self.assertIn((0,0) in make_grid(2,2))
-        self.assertIn((1, 0) in make_grid(2, 2))
-        self.assertIn((0, 1) in make_grid(2, 2))
-        self.assertIn((1, 1) in make_grid(2, 2))
+        self.assertTrue((0,0) in make_grid(2,2))
+        self.assertTrue((1, 0) in make_grid(2, 2))
+        self.assertTrue((0, 1) in make_grid(2, 2))
+        self.assertTrue((1, 1) in make_grid(2, 2))
+
 
     def test_board_starts_empty(self):
         grid = make_grid(2,2)
-        self.assertEquals(grid["(0,0)"] == "0")
-        self.assertEquals(grid["(1,0)"] == "0")
-        self.assertEquals(grid["(0,1)"] == "0")
-        self.assertEquals(grid["(1,1)"] == "0")
+        self.assertEquals(grid[(0,0)], " ")
+        self.assertEquals(grid[(1,0)], " ")
+        self.assertEquals(grid[(0,1)], " ")
+        self.assertEquals(grid[(1,1)], " ")
+
+
+    def test_grid_gets_updated(self):
+        grid = make_grid(2,2)
+        update_grid(grid, (0,0), "U")
+        update_grid(grid, (1, 0), "C")
+        self.assertEquals(grid[(0,0)], "O")
+        self.assertEquals(grid[(1, 0)], "X")
+        self.assertEquals(grid[(1, 1)], " ")
+        self.assertEquals(grid[(0, 1)], " ")
+
+
+    def test_position_is_valid(self):
+        grid = make_grid(2, 2)
+        update_grid(grid, (0, 0), "U")
+
+        self.assertFalse(is_position_valid(grid, (2, 3)))
+        self.assertFalse(is_position_valid(grid, (0,0)))
+        self.assertTrue(is_position_valid(grid, (1,1)))
+
+
     
--- tictactoe.py
+++ tictactoe.py
@@ -5,8 +5,38 @@ def make_grid(x,y):
     :return: a dictionary of coordinates representing a grid of x columns and y rows, all with values 0
     """
 
-    return {(i,j):"0" for i in range(x) for j in range(y)}
+    return {(i,j):" " for i in range(x) for j in range(y)}
 
 
 
 
+def update_grid(grid, played_position, player):
+    """
+    :param grid: a dict of coords representing a tic tac toe board
+    :param played_position: a tuple of 2 integers representing an available position on the grid
+    :param player: "C" for the computer, "U" for the interacting player
+    :return: the updated grid with a "O" on the position for the computer and a "X" for the player
+    """
+
+    if player == "C":
+        grid[played_position] = "X"
+    else:
+        grid[played_position] = "O"
+
+    return grid
+
+
+
+
+def is_position_valid(grid, position):
+    """
+    :param grid: a dict of coords representing a tic tac toe board
+    :param position: a tuple of two integers
+    :return: True if the position is valid and available
+    """
+    if position not in grid:
+        return False
+    if grid[position] != " ":
+        return False
+    else:
+        return True
